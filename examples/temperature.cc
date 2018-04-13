944d7574bea7161239d9b313e5bc11bfb8b073dc
nkhs <a0af@yandex.com>
2018-04-13 23:53:09 +0800

    .

--- examples/temperature.cc
+++ examples/temperature.cc
@@ -97,7 +97,9 @@ int main(int argc, char **argv)
 	
 	//This is how to use the blocking interface. It is very simple. You provide the main 
 	//loop and just hammer on the state machine struct. 
+	cout << "Connecting ... " << endl;
 	gatt.connect_blocking(argv[1]);
+	cout << "Connected with" << argv[1] <<endl;
 	for(;;)
 		gatt.read_and_process_next();
 
